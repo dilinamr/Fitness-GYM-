ec7f3684ea5f952166ff4c737f3a9b983b2fadb2
Dilin-am <dilinam91@gmail.com>
2023-12-24 00:32:18 +0530

    completed

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -19,7 +19,7 @@ const ContactUs = ({ setSelectedPage }: Props) => {
     }
   };
   return (
-    <section id="contactUs" className="mx-auto w-5/6 pt-24 pb-22">
+    <section id="contactus" className="mx-auto w-5/6 pt-24 pb-22">
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
       >
